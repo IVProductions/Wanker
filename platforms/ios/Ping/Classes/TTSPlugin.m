d9a48fdc498a5ba69c1925e4b5844044aac628c2
Simon Hordvik <simon.hordvik@gmail.com>
2014-03-09 00:09:45 -0800

    asd

--- platforms/ios/Ping/Classes/TTSPlugin.m
+++ platforms/ios/Ping/Classes/TTSPlugin.m
@@ -12,19 +12,27 @@
 @implementation TTSPlugin
 
 -(void)speakStuff:(CDVInvokedUrlCommand *)speakString {
+    CDVPluginResult* pluginResult = nil;
     NSString* textToBeSpoken = [speakString.arguments objectAtIndex:0];  //take the incoming speakString object and extract the string
+    if (textToBeSpoken != nil && [textToBeSpoken length] > 0) {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:textToBeSpoken];
+        [self addAlertView:@"success!"];
+    }
+    else {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+        [self addAlertView:@"failure! Input string is empty"];
+    }
     AVSpeechSynthesizer *synthesizer = [[AVSpeechSynthesizer alloc]init];
-    //var synthesizer = new AVSpeechSynthesizer();
     AVSpeechUtterance *utterance = [AVSpeechUtterance speechUtteranceWithString:textToBeSpoken];
     [utterance setRate:0.2f];
     utterance.voice = [AVSpeechSynthesisVoice voiceWithLanguage:@"no-NO"];
     [synthesizer speakUtterance:utterance];
-    [self addAlertView];
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:speakString.callbackId];
 }
 
--(void)addAlertView{
+-(void)addAlertView:(NSString *)message{
     UIAlertView *alertView = [[UIAlertView alloc]initWithTitle:
-                              @"Title" message:@"This is a test alert" delegate:self
+                              @"Title" message:message delegate:self
                                              cancelButtonTitle:@"Cancel" otherButtonTitles:@"Ok", nil];
     [alertView show];
 }
